b03a39364a8cee43733b8586cf4386cee0849fad
Kavinraja.D <119875375+d-kavinraja@users.noreply.github.com>
2023-10-21 09:15:28 +0530

    Create program.py

---
+++ program.py
@@ -0,0 +1,25 @@
+# Register No: 22007928
+# Developed By: Kavinraja D
+# 1-Norm of a Matrix
+
+import numpy as np
+mat =np.array(eval(input()))
+ans=np.linalg.norm(mat,1)
+norm_of_matrix = "{:.2f}".format(ans)
+print(norm_of_matrix)
+
+# 2-Norm of a Matrix
+
+import numpy as np
+mat=np.array(eval(input()))
+ans=np.linalg.norm(mat,2)
+norm_of_matrix='{:.2f}'.format(ans)
+print(norm_of_matrix)
+
+# Infinity Norm of a Matrix
+
+import numpy as np
+mat=np.array(eval(input()))
+ans=np.linalg.norm(mat,np.inf)
+norm_of_matrix="{:.2f}".format(ans)
+print(norm_of_matrix)
